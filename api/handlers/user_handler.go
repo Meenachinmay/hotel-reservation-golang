7174aa052935d44e411cb906aec21b71d77916e8
meenachinmay <Chinmayanand896@gmail.com>
2024-01-18 04:56:41 +0900

    database layer added, user can be store, user can be fetched

--- api/handlers/user_handler.go
+++ api/handlers/user_handler.go
@@ -1,16 +1,39 @@
 package api
 
 import (
+	"context"
+
+	"github.com/Meenachinmay/hotel-reservation-golang/db"
 	"github.com/Meenachinmay/hotel-reservation-golang/types"
 	"github.com/gofiber/fiber/v2"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+		id := c.Params("id")
+		ctx := context.Background()
+		user, err := h.userStore.GetUserByID(ctx, id)
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	u := types.User{
 		FirstName: "Chinmay",
 		LastName: "anand",
 	}
 
 	return c.JSON(u)
-
 }
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	api "github.com/Meenachinmay/hotel-reservation-golang/api/handlers"
+	"github.com/Meenachinmay/hotel-reservation-golang/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dbURI = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const collection_name = "users"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbURI))
+	if err != nil {
+		log.Fatal(err) 
+	}
+
+	// handlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
     app := fiber.New()
 	apiv1 := app.Group("/api/v1")
 
-    apiv1.Get("/user", api.HandleGetUsers)
+    apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
     app.Listen(":3000")
 }
